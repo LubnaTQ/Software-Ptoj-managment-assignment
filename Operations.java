3c82213a912ce16321e7002880c52a36f3376640
YasmeenAlessam <202101100@pmu.edu.sa>
2026-05-05 00:06:05 +0300

    Add Calculator class with multiply and logicalAnd methods

--- Operations.java
+++ Operations.java
@@ -1,1 +1,21 @@
+public class Calculator {
 
+    // Method for multiplication
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method for logical AND (comparison)
+    public static boolean logicalAnd(int a, int b) {
+        return (a > 0 && b > 0);
+    }
+
+    public static void main(String[] args) {
+
+        int product = multiply(6, 7);
+        System.out.println("Multiplication: " + product);
+
+        boolean result = logicalAnd(5, 3);
+        System.out.println("Logical AND: " + result);
+    }
+}
